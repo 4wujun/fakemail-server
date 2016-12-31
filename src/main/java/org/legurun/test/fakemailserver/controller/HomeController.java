5ddf31ae78f6b54dd32a8127ab0a0c26458a1e3c
patlenain <patrice@legurun.org>
2016-12-31 14:16:00 +0100

    Remove view

--- src/main/java/org/legurun/test/fakemailserver/controller/HomeController.java
+++ src/main/java/org/legurun/test/fakemailserver/controller/HomeController.java
@@ -2,11 +2,14 @@ package org.legurun.test.fakemailserver.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/")
 public class HomeController {
+
 	@GetMapping(value="/")
-	public String home() {
-		return "home";
+	public String index() {
+		return "index";
 	}
 }
