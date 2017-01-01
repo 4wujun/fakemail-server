91e67656faadd33cbc749dd38688b0194d0c7e89
patlenain <patrice@legurun.org>
2017-01-01 14:47:07 +0100

    Add hibernate configuration

--- src/main/java/org/legurun/test/fakemailserver/config/AppInitializer.java
+++ src/main/java/org/legurun/test/fakemailserver/config/AppInitializer.java
@@ -10,7 +10,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[] { RootConfig.class, SubEthaSMTPConfig.class };
+		return new Class<?>[] { RootConfig.class, RepositoryConfig.class, SubEthaSMTPConfig.class };
 	}
 
 	@Override
---
+++ src/main/java/org/legurun/test/fakemailserver/config/RepositoryConfig.java
@@ -0,0 +1,70 @@
+package org.legurun.test.fakemailserver.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages="org.legurun.test.fakemailserver.dao")
+public class RepositoryConfig {
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+		ds.setUrl(environment.getRequiredProperty("jdbc.url"));
+		ds.setUsername(environment.getRequiredProperty("jdbc.username"));
+		ds.setPassword(environment.getRequiredProperty("jdbc.password"));
+		return ds;
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager htm = new HibernateTransactionManager();
+		htm.setSessionFactory(sessionFactory);
+		return htm;
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTemplate hibernateTemplate(SessionFactory sessionFactory) {
+		HibernateTemplate hibernateTemplate = new HibernateTemplate(sessionFactory);
+		return hibernateTemplate;
+	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(getDataSource());
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		sessionFactory.setPackagesToScan("org.legurun.test.fakemailserver.model");
+		return sessionFactory;
+	}
+
+	@Bean
+	public Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+		properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+		return properties;
+	}
+}
