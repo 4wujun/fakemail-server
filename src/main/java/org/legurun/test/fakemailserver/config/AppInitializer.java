1b2c27a03445ae47c614b206631e2e33034456b1
patlenain <patrice@legurun.org>
2017-01-15 16:09:50 +0100

    Affichage du nom du fichier de configuration externe

--- src/main/java/org/legurun/test/fakemailserver/config/AppInitializer.java
+++ src/main/java/org/legurun/test/fakemailserver/config/AppInitializer.java
@@ -1,12 +1,17 @@
 package org.legurun.test.fakemailserver.config;
 
 import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	private static final Logger LOG = LoggerFactory.getLogger(AppInitializer.class);
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
@@ -26,5 +31,11 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	@Override
 	protected Filter[] getServletFilters() {
 		return new Filter[] { new HiddenHttpMethodFilter(), new CharacterEncodingFilter() };
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		LOG.info("Use external configuration file " + System.getProperty("externalConfigurationLocation"));
+		super.onStartup(servletContext);
 	}
 }
