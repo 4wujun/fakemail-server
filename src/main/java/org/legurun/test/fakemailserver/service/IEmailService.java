695597c7a7fdbb1cddc3a519a44cba222b39ae51
patlenain <patrice@legurun.org>
2017-02-02 18:20:19 +0100

    Simplification de l'interface du service mail

--- src/main/java/org/legurun/test/fakemailserver/service/IEmailService.java
+++ src/main/java/org/legurun/test/fakemailserver/service/IEmailService.java
@@ -1,21 +1,12 @@
 package org.legurun.test.fakemailserver.service;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 import org.legurun.test.fakemailserver.model.Email;
-import org.subethamail.smtp.TooMuchDataException;
 import org.subethamail.smtp.helper.SimpleMessageListener;
 
 public interface IEmailService extends SimpleMessageListener {
 
 	List<Email> list();
-
-	@Override
-	boolean accept(String from, String recipient);
-
-	@Override
-	void deliver(String from, String recipient, InputStream data) throws TooMuchDataException, IOException;
 
 }
