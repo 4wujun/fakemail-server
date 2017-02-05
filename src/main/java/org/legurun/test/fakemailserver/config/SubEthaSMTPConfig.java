aa4f8e325216918b933a55ee15676390d2799e61
patlenain <patrice@legurun.org>
2017-02-05 19:19:01 +0100

    Configuration du port/hote d'écoute

--- src/main/java/org/legurun/test/fakemailserver/config/SubEthaSMTPConfig.java
+++ src/main/java/org/legurun/test/fakemailserver/config/SubEthaSMTPConfig.java
@@ -1,6 +1,7 @@
 package org.legurun.test.fakemailserver.config;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import org.legurun.test.fakemailserver.service.IEmailService;
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.subethamail.smtp.MessageHandlerFactory;
 import org.subethamail.smtp.helper.SimpleMessageListenerAdapter;
@@ -19,13 +21,21 @@ public class SubEthaSMTPConfig {
 	private static final Logger LOG = LoggerFactory.getLogger(SubEthaSMTPConfig.class);
 
 	@Autowired
+	private Environment environment;
+
+	@Autowired
 	@Bean(initMethod="start", destroyMethod="stop")
-	public SMTPServer smtpServer(IEmailService mailService) {
+	public SMTPServer smtpServer(IEmailService mailService) throws UnknownHostException {
 		LOG.trace("Initialisation smtpServer");
 		MessageHandlerFactory factory = new SimpleMessageListenerAdapter(mailService);
+
+		String listenHost = environment.getRequiredProperty("smtp.listen.host");
+		int listenPort = environment.getRequiredProperty("smtp.listen.port", Integer.class);
+		LOG.debug("Listening on {}:{}", listenHost, listenPort);
 		SMTPServer server = new SMTPServer(factory);
-		server.setBindAddress(InetAddress.getLoopbackAddress());
-		server.setPort(10025);
+		server.setSoftwareName("Fakemail Server");
+		server.setBindAddress(InetAddress.getByName(listenHost));
+		server.setPort(listenPort);
 		return server;
 	}
 }
