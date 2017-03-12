ef6d085e73627f9167df7d0d3f4d265de8dfbcf6
patlenain <patrice@legurun.org>
2017-03-12 16:25:46 +0100

    Remove JSON date format - problems with timezone

--- src/main/java/org/legurun/test/fakemailserver/config/WebMvcConfig.java
+++ src/main/java/org/legurun/test/fakemailserver/config/WebMvcConfig.java
@@ -1,7 +1,5 @@
 package org.legurun.test.fakemailserver.config;
 
-import java.util.List;
-
 import org.legurun.test.fakemailserver.utils.SortOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,17 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Configuration
 @EnableWebMvc
@@ -52,19 +45,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 		configurer.enable();
 	}
 
-	@Override
-	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-		LOG.trace("Try to configure JSON date format ...");
-		for (HttpMessageConverter<?> converter : converters) {
-			if (converter instanceof MappingJackson2HttpMessageConverter) {
-				LOG.trace("JSON formatter found ! Configuring it");
-				MappingJackson2HttpMessageConverter c = (MappingJackson2HttpMessageConverter) converter;
-				ObjectMapper objectMapper = c.getObjectMapper();
-				objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-			}
-		}
-		super.extendMessageConverters(converters);
-	}
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
 		registry.addConverter(new SortOrder.OrderConverter());
