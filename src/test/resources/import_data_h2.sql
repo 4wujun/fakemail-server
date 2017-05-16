5634fc257edd7df114de7f33b708518cb109ab03
patlenain <patrice@legurun.org>
2017-05-16 20:15:28 +0200

    Set the license to LGPL3

--- src/test/resources/import_data_h2.sql
+++ src/test/resources/import_data_h2.sql
@@ -1,3 +1,24 @@
+---
+-- #%L
+-- Fakemail server
+-- %%
+-- Copyright (C) 2017 Patrice Le Gurun
+-- %%
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Lesser Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Lesser Public
+-- License along with this program.  If not, see
+-- <http://www.gnu.org/licenses/lgpl-3.0.html>.
+-- #L%
+---
 
 insert into sender (version, date_created, last_updated, address) values (0, current_timestamp, current_timestamp, 'patrice@legurun.org');
 insert into sender (version, date_created, last_updated, address) values (0, current_timestamp, current_timestamp, 'root@legurun.org');
